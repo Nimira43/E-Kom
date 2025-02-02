269d338c5251e9da0e31f6e39e5b47497f61440b
Nimira43 <n.rabone72@aol.com>
2025-02-02 15:20:19 +0000

    Adding styles to the CartDrawer including open and close positions.

--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react'
+
 const CartDrawer = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen)
+  }
+
   return (
-    <div>
-      Cart Drawer
+    <div 
+      className={
+        `fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-grey-extra-light shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+          drawerOpen ?
+          'translate-x-0' :
+          'translate-x-full'
+        }`
+      }
+
+    >
+      
     </div>
   )
 }
