7c9cbe7e95623d48d644d56d95a1bc8c882ece2b
Nimira43 <n.rabone72@aol.com>
2025-02-02 10:41:58 +0000

    Set up Tailwind 3 and customised theme colours in tailwind.config.js

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -2,16 +2,16 @@ import { RiFacebookLine, RiTwitterXLine, RiInstagramLine } from "react-icons/ri"
 
 const Topbar = () => {
   return (
-    <div className='bg-[#777] text-[#f5fffa]'>
+    <div className='bg-grey-medium text-light'>
       <div className='container mx-auto flex justify-between items-center'>
         <div className='flex items-center space-x-4 p-2'>
-          <a href="#" className='hover:text-[#c7ecfa]'>
+          <a href="#" className='hover:text-blue-light'>
             <RiFacebookLine className='h-5 w-5'/>            
           </a>
-          <a href="#" className='hover:text-[#c7ecfa]'>
+          <a href="#" className='hover:text-blue-light text-royal'>
             <RiTwitterXLine className='h-5 w-5'/>            
           </a>
-          <a href="#" className='hover:text-[#c7ecfa]'>
+          <a href="#" className='hover:text-blue-light'>
             <RiInstagramLine className='h-5 w-5'/>            
           </a>
         </div>
@@ -19,7 +19,7 @@ const Topbar = () => {
           <span>Our expert team are here to answer your queries.</span>
         </div>
         <div className="text-sm">
-          <a href="#" className='hover:text-[#c7ecfa]'>+44 (0)121 328 4832</a>
+          <a href="#" className='hover:text-blue-light'>+44 (0)121 328 4832</a>
         </div>
       </div>      
     </div>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-text': '#444',
+        'dark': '#111',
+        'light': '#f5fffa',
+        'blue-dark': '',
+        'blue-medium': '',
+        'blue-light': '#c7ecfa',
+        'grey-extra-light': '#eee',
+        'grey-light': '#ccc',
+        'grey-medium': '#777',
+        'grey-dark': '#333',
+      },
+    },
+  },
+  plugins: [],
+}
