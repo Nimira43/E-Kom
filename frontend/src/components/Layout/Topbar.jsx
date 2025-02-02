7d0d977345b6ce0c76892ffa6205244b1ae12226
Nimira43 <n.rabone72@aol.com>
2025-02-02 09:51:25 +0000

    Added social icons to Topbar

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -1,7 +1,21 @@
+import { RiFacebookLine, RiTwitterXLine, RiInstagramLine } from "react-icons/ri";
+
 const Topbar = () => {
   return (
-    <div className='bg-[#ccc]'>
-      Topbar      
+    <div className='bg-[#777] text-[#f5fffa]'>
+      <div className='container mx-auto'>
+        <div>
+          <a href="#" className='hover:text-[#c7ecfa]'>
+            <RiFacebookLine className='h-5 w-5'/>            
+          </a>
+          <a href="#" className='hover:text-[#c7ecfa]'>
+            <RiTwitterXLine className='h-5 w-5'/>            
+          </a>
+          <a href="#" className='hover:text-[#c7ecfa]'>
+            <RiInstagramLine className='h-5 w-5'/>            
+          </a>
+        </div>
+      </div>      
     </div>
   )
 }
