606bf44f86a38d4642713e10902261955af6eb5b
Nimira43 <n.rabone72@aol.com>
2025-02-02 12:10:47 +0000

    Working on rendering the search bar component with a toggle feature. When the isOpen state is true, the search bar is displayed; otherwise, a search button is shown.

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -10,9 +10,26 @@ const SearchBar = () => {
   }
 
   return (
-    <div>
+    <div 
+      className={
+        `flex items-center justify-center w-full transition-all duration-300 ${
+          isOpen ? 
+          'absolute top-0 left-0 w-full bg-light h-24 z-50' : 
+          'w-auto'
+        }`
+      }
+    >
       {isOpen ? (
-        <form></form>
+        <form className="relative flex items-center justify-center w-full">
+          <div className="relative w-1/2">
+            <input 
+              type="text" 
+              placeholder="Search..." 
+              value={searchTerm} 
+              className="bg-light px-4 py-2 border-2 border-grey-light rounded-md focus:outline-none w-full"
+            />
+          </div>
+        </form>
       ) : (
         <button onClick={handleSearchToggle}>
           <RiSearchLine className="h-6 w-6"/>
