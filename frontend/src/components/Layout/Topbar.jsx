ffb12c6ea25a0116de3ae8f165269950dda493fb
Nimira43 <n.rabone72@aol.com>
2025-02-02 10:46:05 +0000

    Tidy up.

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -1,25 +1,25 @@
-import { RiFacebookLine, RiTwitterXLine, RiInstagramLine } from "react-icons/ri";
+import { RiFacebookLine, RiTwitterXLine, RiInstagramLine } from 'react-icons/ri';
 
 const Topbar = () => {
   return (
     <div className='bg-grey-medium text-light'>
       <div className='container mx-auto flex justify-between items-center'>
         <div className='flex items-center space-x-4 p-2'>
-          <a href="#" className='hover:text-blue-light'>
+          <a href='#' className='hover:text-blue-light'>
             <RiFacebookLine className='h-5 w-5'/>            
           </a>
-          <a href="#" className='hover:text-blue-light text-royal'>
+          <a href='#' className='hover:text-blue-light text-royal'>
             <RiTwitterXLine className='h-5 w-5'/>            
           </a>
-          <a href="#" className='hover:text-blue-light'>
+          <a href='#' className='hover:text-blue-light'>
             <RiInstagramLine className='h-5 w-5'/>            
           </a>
         </div>
         <div className='text-sm text-center'>
           <span>Our expert team are here to answer your queries.</span>
         </div>
-        <div className="text-sm">
-          <a href="#" className='hover:text-blue-light'>+44 (0)121 328 4832</a>
+        <div className='text-sm'>
+          <a href='#' className='hover:text-blue-light'>+44 (0)121 328 4832</a>
         </div>
       </div>      
     </div>
